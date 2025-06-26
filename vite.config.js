d4f9b1e5ed7cd864f55d559425b92cb5c2d9fd82
Leo Cazenille <leo.cazenille@gmail.com>
2025-06-26 15:57:58 +0200

    rename -> GUESS-THE-KILLER

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/supabase-vote-killer/",  //  <-- repo name with trailing slash
+  base: "/GUESS-THE-KILLER/",  //  <-- repo name with trailing slash
   plugins: [react()],
 });
